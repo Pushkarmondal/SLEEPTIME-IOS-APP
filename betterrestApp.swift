99423877bff9892824e31647adbbdf1c26d9566e
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2022-12-17 16:56:08 +0530

    Add files via upload

---
+++ betterrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterrestApp.swift
+//  betterrest
+//
+//  Created by pushkar mondal on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct betterrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
